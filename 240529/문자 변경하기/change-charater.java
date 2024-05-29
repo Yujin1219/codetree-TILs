566521da3d6b4fcb00a152533315cd8545133f83
Yujin1219 <127809173+Yujin1219@users.noreply.github.com>
2024-05-29 15:49:15 +0900

    Title: 문자 변경하기, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

--- 240529/문자 변경하기/change-charater.java
+++ 240529/문자 변경하기/change-charater.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        Char a = 'C';
+        char a = 'c';
         a='T';
         System.out.print(a);
     }
