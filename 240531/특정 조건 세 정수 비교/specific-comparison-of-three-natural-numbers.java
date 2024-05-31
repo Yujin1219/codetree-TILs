709919f8189982b880352755fd74be6ffe7287d8
Yujin1219 <127809173+Yujin1219@users.noreply.github.com>
2024-05-31 17:50:21 +0900

    Title: 특정 조건 세 정수 비교, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240531/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
+++ 240531/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -8,8 +8,8 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
         if(a<=b && a<=c){
-            System.out.print("1");
-        }else System.out.print("0");
+            System.out.print("1 ");
+        }else System.out.print("0 ");
         if(a==b && b==c){
             System.out.println("1");
         }else System.out.println("0");
