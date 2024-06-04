402f9df9dad95bc8d7ad9080a0c2aa75114f3337
Yujin1219 <127809173+Yujin1219@users.noreply.github.com>
2024-06-04 16:47:59 +0900

    Title: 369 게임, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240604/369 게임/369-game.java
+++ 240604/369 게임/369-game.java
@@ -6,19 +6,19 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
-
         for(int i = 1 ; i <=n ; i++){
-            int f = i/10;
-            int s = i%10;
-            if(i < 10){
-                if((i %3 == 0)){
+            int f = i / 10;
+            int s = i % 10;
+
+            if(i < 10 ){
+                if(i %3 == 0){
                 System.out.print("0 ");
                 } 
                 else{
                 System.out.print(i + " ");
                 }
             }
-            else if(i >= 10){
+            else {
                 if((i %3 == 0) || (f % 3 ==0) || (s % 3 ==0)){
                 System.out.print("0 ");    
                 }
