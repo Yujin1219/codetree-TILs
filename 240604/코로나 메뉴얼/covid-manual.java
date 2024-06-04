5083fb6fad1ec54a14e4a77b4fd82077c0a328f1
Yujin1219 <127809173+Yujin1219@users.noreply.github.com>
2024-06-04 10:42:12 +0900

    Title: 코로나 메뉴얼, Time: 127ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240604/코로나 메뉴얼/covid-manual.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc= new Scanner(System.in);
+        char p = sc.next().charAt(0); //체온
+        int temp = sc.nextInt();
+        char q; 
+        int e = 0;//emergency
+        for(int i = 1; i<=3;i++){
+        if(p == 'Y'){
+            if(temp >=37){
+                q = 'A';
+                e++;
+                } else q = 'C';
+        }
+        else{
+            if(temp >=37) q = 'B';
+            else q = 'D';
+        }
+        }
+        if(e >=2) System.out.println("E");
+        else System.out.println("N");
+    }
+}
