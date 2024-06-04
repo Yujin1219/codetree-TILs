279a4c7720774ecab4a437db59604635f2b1c6fe
Yujin1219 <127809173+Yujin1219@users.noreply.github.com>
2024-06-04 10:25:36 +0900

    Title: 최댓값 구하기, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

--- 240604/최댓값 구하기/maximum-value.java
+++ 240604/최댓값 구하기/maximum-value.java
@@ -8,12 +8,12 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        if(a > b){
-            if(c < a) System.out.println(a);
+        if(a >= b){
+            if(c <= a) System.out.println(a);
             else System.out.println(c);
         }
-        else if(b>a){
-            if(c<b) System.out.println(b);
+        else if(b>=a){
+            if(c<=b) System.out.println(b);
             else System.out.println(c);
         }
         else if(c > a && c > b)System.out.println(c);
