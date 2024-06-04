b006a21aca6d8f86372d25d1f107a37a953ab5d8
Yujin1219 <127809173+Yujin1219@users.noreply.github.com>
2024-06-04 10:26:06 +0900

    Title: 최댓값 구하기, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240604/최댓값 구하기/maximum-value.java
+++ 240604/최댓값 구하기/maximum-value.java
@@ -16,6 +16,5 @@ public class Main {
             if(c<=b) System.out.println(b);
             else System.out.println(c);
         }
-        else if(c > a && c > b)System.out.println(c);
-    }
+            }
 }
